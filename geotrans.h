4f42df02b09e8063fbffd2c337c2435e631ede13
pauly.joerg@googlemail.com <pauly.joerg@googlemail.com@400205d0-1850-6dd4-e755-e13897e3bdd5>
2015-01-25 12:54:47 +0000

    Creation

---
+++ geotrans.h
@@ -0,0 +1,53 @@
+#ifndef GEOTRANS_H
+#define GEOTRANS_H
+
+#include <QMainWindow>
+#include <QSettings>
+#include <QtPositioning/QGeoCoordinate>
+#include <QtPositioning/QGeoPositionInfoSource>
+#include <QGeoSatelliteInfoSource>
+#include <QtNetwork/QNetworkAccessManager>
+#include <QtNetwork/QUdpSocket>
+#include <QtNetwork/QHostAddress>
+#include "nmea/nmea.h"
+#include <QFileDialog>
+#include "cnmeasource.h"
+#include "cnmeagen.h"
+
+
+namespace Ui {
+class GeoTrans;
+}
+
+class GeoTrans : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit GeoTrans(QWidget *parent = 0);
+    ~GeoTrans();
+
+private slots:
+    void on_cmdQuit_clicked();
+    void on_cmdStart_clicked();
+    void on_Update(const QGeoPositionInfo &update);
+    void on_pushButton_clicked();
+    void on_SatUseUpdate(QList<QGeoSatelliteInfo> llist);
+    void on_SatViewUpdate(QList<QGeoSatelliteInfo> llist);
+
+private:    
+    Ui::GeoTrans *ui;
+    QSettings* m_settings;
+    QWidget *m_parent;
+    QGeoPositionInfoSource *m_sat;
+    QGeoSatelliteInfoSource *m_satinf;
+    QHostAddress *m_host;
+    QUdpSocket *m_socket;         
+    QString* m_filename;
+    CNmeaSource *m_src;
+    CNmeaGen *m_nmea;
+    QList<QGeoSatelliteInfo> m_inView;
+    QList<QGeoSatelliteInfo> m_inUse;
+};
+
+#endif // GEOTRANS_H
